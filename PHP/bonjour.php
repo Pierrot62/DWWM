4cc7ada6581a2d846c22be81a3f611eaa0c5f964
Pierrot62 <courquin.pierre62@gmail.com>
2020-09-14 15:46:07 +0200

    V0

---
+++ PHP/bonjour.php
@@ -0,0 +1,49 @@
+
+<?php 
+
+   // $bonjour = "Bonjour tout le monde \n";
+   // echo $bonjour;
+
+   // print $bonjour;
+
+   // $note1 = 5;
+   //    $note2 = 78;
+   // $note3 = 9;
+
+   // $somme = $note1 + $note2 + $note3;
+
+   // echo $somme ;
+   
+   echo $_SERVER["SERVER_ADDR"];
+
+
+   // $a = 1;
+   // while ($a < 151) {
+   //    echo "$a";
+   //    $a++;
+   // }
+
+   
+      
+
+
+   $a = 1;
+   while ($a < 500) {
+      echo "Je dois faire des sauvegardes regulieres de mes fichiers \n";
+      $a++;
+   }
+
+   ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   
+</body>
+</html>
