a6f061967de5b65251238b6d691bd3bc3b9efbae
59011-95-03 <59011-95-03@exchange.ad.afpanet>
2020-10-12 11:33:21 +0200

    up

---
+++ EVALUTATION/Eval_POO_Pierre/Joueur.Class.php
@@ -0,0 +1,75 @@
+<?php
+
+ Class Joueur {
+	/***************************************** Attributs **********************************************/
+
+	private $_PointDeVie;
+
+	/***************************************** Accesseurs **********************************************/
+	
+	public function getPointDeVie()
+	{
+		return $this->_PointDeVie;
+	}
+
+	public function setPointDeVie($PointDeVie)
+	{
+		return $this->_PointDeVie = $PointDeVie;
+	}
+
+	/***************************************** Constructeur **********************************************/
+
+	public function __construct(array $options = [])
+	{
+		if (!empty($options)) // empty : renvoi vrai si le tableau est vide
+		{
+			$this->hydrate($options);
+		}
+	}
+	public function hydrate($data)
+	{
+		foreach ($data as $key => $value)
+		{
+			$methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
+			if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
+			{
+				$this->$methode($value);
+			}
+		}
+	}
+	/***************************************** Methode **********************************************/
+
+	/**
+	* Transforme l'objet en chaine de caractères
+	*
+	* @return String
+	*/
+	public function toString(){
+		return "PointeVie : ".$this->getPointDeVie()	;
+	}
+
+	/**
+	* Renvoi vrai si l'objet en paramètre est égal à l'objet appelant
+	*
+	* @param [type] obj
+	* @return bool
+	*/
+	public function equalsTo(){
+		return  "";
+	}
+
+	/**
+	* Compare 2 objets
+	* Renvoi 1 si le 1er est >
+	*        0 si ils sont égaux
+	*        -1 si le 1er est <
+	*
+	* @param [type] obj1
+	* @param [type] obj2
+	* @return void
+	*/
+	public function compareTo(){
+		return "";
+	}
+}
+
