b3ba77642847bb66f55a2e9aaa989b907a981f4f
59011-95-03 <59011-95-03@exchange.ad.afpanet>
2020-10-21 12:08:44 +0200

    21/10/2020 12:08:43,74

--- SQL/Exo requete/4. Exercice 2.sql
+++ SQL/Exo requete/4. Exercice 2.sql
@@ -2,19 +2,28 @@ executer les 2 instructions suivantes sur la base
 ALTER TABLE commandes ADD cde_total int;
 UPDATE commandes SET cde_total = quantiteCommande * (select prixArticle from articles where articles.idArticle = commandes.idArticle)
 
-Affichez le contenu de la table commande. Qu'y a-t-il de changé dans cette table ? Comment le total de la
+Affichez le contenu de la table commande. Qu y a-t-il de changé dans cette table ? Comment le total de la
 commande a-t-il été calculé ? 
 
 Ecrire des requêtes SELECT. Vous vérifierez que le résultat de la requête correspond à votre inspection des
 tables correspondantes. 
 A)Afficher le montant le plus élevé de commande.
+SELECT MAX(cde_total), `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient
 B) Afficher le montant moyen des commandes.
+SELECT AVG(cde_total), `nomClient` , `prenomClient` FROM `commandes`
 C) Afficher le montant le plus bas de commande.
+SELECT MIN(cde_total), `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient
 D) Afficher le nombre de commandes qui ont été passées.
+SELECT SUM(cde_total) AS "NOMBRE DE COMMANDE" FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient
 E) Afficher le montant moyen de commande par client
+SELECT ROUND(AVG(cde_total),2), `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient GROUP BY `clients` . `idClient`
 F) Afficher le montant le plus élevé de commande par client.
+SELECT ROUND(MAX(cde_total),2), `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient GROUP BY `clients` . `idClient`
 G) Afficher le nombre de commandes par client.
-H) Afficher le nombre d 'articles commandés en moyenne par client
+SELECT 'quantiteCommande', `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient GROUP BY `clients` . `idClient`
+H) Afficher le nombre d 'articles commandés en moyenne par client.
+SELECT 'quantiteCommande', `nomClient` , `prenomClient` FROM `commandes` INNER JOIN `clients` ON commandes.idCommande = clients.idClient GROUP BY `clients` . `idClient`
+
 I) Afficher le nombre d'articles commandés en moyenne par article.
 J) Afficher le nombre total d'articles commandés par article.
 K) Afficher le nombre moyen d'articles par client et par date.
