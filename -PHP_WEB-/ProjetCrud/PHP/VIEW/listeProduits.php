c5b7d18bddd688f57fc4a8610e4f626538399ab3
Pierrot62 <courquin.pierre62@gmail.com>
2020-11-19 18:46:43 +0100

    19/11/2020 18:46:43,47

--- -PHP_WEB-/ProjetCrud/PHP/VIEW/listeProduits.php
+++ -PHP_WEB-/ProjetCrud/PHP/VIEW/listeProduits.php
@@ -1,19 +1,21 @@
 <?php
 echo '
 <section>
-<div><a href="PHP/VIEW/add.php">Ajouter un produit</a></div>
+<div><a class="new" href="PHP/VIEW/add.php">Ajouter un produit</a></div>
 ';
-    echo "liste des Produits" . '<br>';
+    echo "<div class=\"titrePage\">Liste des Produits</div>" . '<br>';
 $tableau = ProduitsManager::getList();
 foreach ($tableau as $unProduit)
 {
     echo'
+    <div></div>
     <div class="produit">
-        <div>'.$unProduit->getLibelleProduit().'</div>
-        <div><a href="PHP/VIEW/view.php?id='.$unProduit->getIdProduit().'">Voir le produit</a></div>
-        <div><a href="PHP/VIEW/update.php?id='.$unProduit->getIdProduit().'">Modifier le produit</a></div>
-        <div><a href="PHP/VIEW/delete.php?id='.$unProduit->getIdProduit().'">Supprimer le produit</a></div>
-    </div>';
+        <div class="nomProduit">'.$unProduit->getLibelleProduit().'</div>
+        <div><a class="voir" href="PHP/VIEW/view.php?id='.$unProduit->getIdProduit().'">Voir le produit</a></div>
+        <div><a class="edit" href="PHP/VIEW/update.php?id='.$unProduit->getIdProduit().'">Modifier le produit</a></div>
+        <div><a class="supp" href="PHP/VIEW/delete.php?id='.$unProduit->getIdProduit().'">Supprimer le produit</a></div>
+    </div>
+    <div></div>';
 }
 
 
--- -PHP_WEB-/ProjetCrud/PHP/VIEW/traitementAdd.php
+++ -PHP_WEB-/ProjetCrud/PHP/VIEW/traitementAdd.php
@@ -1,11 +1,9 @@
 <?php
 include ("head.php");
  
-
 //TRAITEMENT  DU FORMULAIRE//
 $prix=intval($_GET['prix']);
-$produit=new Produits
-ProduitsManager::update($produit);r
-
+$produit=new Produits(["libelleProduit"=>$_GET["libelleProduit"],"prix"=>$prix,"dateDePeremption"=>$_GET["dateDePeremption"]]);
+ProduitsManager::add($produit);
 
 header("location: ../../index.php");
