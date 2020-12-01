81bb124e1968fc73c3814997acec592ce129ed88
Pierrot62 <courquin.pierre62@gmail.com>
2020-12-01 18:36:30 +0100

    01/12/2020 18:36:30,05

---
+++ -PHP_WEB-/ConnectionMultilangue/PHP/VIEW/nav.php
@@ -0,0 +1,15 @@
+<nav>
+<div class="espace"></div>
+
+
+<div class="lang"> 
+<?php echo texte('langue disponible'); ?> 
+<a href="<?php echo $uri; ?>lang=EN">
+    <img src="./IMG/flagEn.png" alt="drapeaux anglais">
+</a>
+
+<a href="<?php echo $uri; ?>lang=FR">
+    <img src="./IMG/flagFr.png" alt="drapeaux francais">
+</a>
+</div>    
+</nav>
