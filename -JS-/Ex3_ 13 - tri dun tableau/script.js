5fde2d15cc919bcbdd7f8cd3f2486f7ce45e9251
Pierrot62 <courquin.pierre62@gmail.com>
2020-12-11 12:15:02 +0100

    11/12/2020 12:15:01,99

--- -JS-/Ex3_ 13 - tri dun tableau/script.js
+++ -JS-/Ex3_ 13 - tri dun tableau/script.js
@@ -1,8 +1,20 @@
-  
-var tableau = ["pierre", "paul", "coco","toto"];
+var tableau = new Array(7879, 87, 567, 78673, 2, 78, 487, 24, 46, 84, 56, 15598);
 
-var index = prompt("prenom a trouver :  ");
-    var prenom = tableau.indexOf(index);
-    tableau.splice(prenom, 1);
-    tableau.push("");
-    console.log(tableau);
+for (var i = 0; i < tableau.length; i++) {
+
+    var temp;
+
+    for (var x = 0; x < tableau.length; x++) {
+
+        if (tableau[x] > tableau[i]) {
+            temp = tableau[x];
+            tableau[x] = tableau[i];
+            tableau[i] = temp;
+
+        }
+    }
+
+}
+for (var i = 0; i < tableau.length; i++) {
+    console.log(tableau[i] + "\n")
+}
