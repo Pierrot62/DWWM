36f965b23f7fa6cefd47d70bc21de556d4900591
Pierrot62 <courquin.pierre62@gmail.com>
2020-12-17 17:15:02 +0100

    17/12/2020 17:15:02,46

--- -JS-/Calculatrice/script.js
+++ -JS-/Calculatrice/script.js
@@ -1,19 +1,22 @@
+var valeurButton = document.getElementsByTagName("button");
 
-var buttonCliquer = document.getElementsByClassName("button");
-
-console.log(buttonCliquer);
-
-for (let i = 0; i < buttonCliquer.length; i++) {
-
-    buttonCliquer[i].addEventListener("click", calcul);
- 
+for (let i = 0; i < valeurButton.length; i++) {
+    valeurButton[i].addEventListener("click", clickBouton);
 }
 
+var afficheur = document.getElementById("resultat");
 
-function calcul(e) {
-    var valeur = "";
-    valeur = e.target.id;
-    var = valeur;
-    console.log(valeur);
-
+function clickBouton(e) {
+    var boutonCliquer = e.target;
+    var valeur = boutonCliquer.textContent;
+    if (valeur == "-") {
+        afficheur.textContent = eval(affiche.textContent);
+    } else if (valeur == "clear") {
+        afficheur.textContent = eval(afficheur.textContent);
+    } else if (valeur == "supprimer") {
+        afficheur = afficheur.textContent;
+        afficheur.textContent = afficheur.substring(0, afficheur.length - 1);
+    } else {
+        afficheur.textContent += valeur;
+    }
 }
