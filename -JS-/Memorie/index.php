e5e3946f71545897b48a08ced65a892b96aeae2c
59011-95-03 <59011-95-03@exchange.ad.afpanet>
2020-12-23 12:20:04 +0100

    23/12/2020 12:20:03,59

--- -JS-/Memorie/index.php
+++ -JS-/Memorie/index.php
@@ -7,13 +7,16 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
-<div>
-    <h1>Jeu du Memorie</h1>
+<div id="gagner">Bravo vous avez gagné !</div>
+<h1>Jeu du Memory</h1>
+<div class="menu">
     <button id="solution">Solution</button>
     <button id="reset">Reset</button>
+    <div id="nbPaire"></div>
+    <div id="nbCoup"></div>
+    <div id="chrono">ezrzr</div>
+    <!-- <div class="joueur">C'est au joueur 1 de jouer !</div> -->
 </div>
-
 
 
 
@@ -26,7 +29,6 @@ for ($i = 1; $i <= 8; $i++) {
 $compteur = 1;
 for ($a = 1; $a < 5; $a++) {
 
-    echo '<div class="espaceHaut"></div>';
     echo '<div class="espace"></div>';
     echo '<div class="ligne">';
 
--- -JS-/Memorie/script.js
+++ -JS-/Memorie/script.js
@@ -1,34 +1,65 @@
 var recto = document.getElementsByClassName("recto");
+var verso = document.getElementsByClassName("verso");
+var afficheGagner = document.getElementById("gagner");
+var buttonReset = document.getElementById("reset");
+var button = document.getElementById("solution");
+var affichePaire = (document.getElementById("nbPaire").innerHTML = "Nombre de paire trouver : 0");
+var afficheNbCoup = (document.getElementById("nbCoup").innerHTML = "Nombre de clics : 0");
+var chrono = document.getElementById("chrono");
+
 var etat = true;
+var sol = false;
 var check = [];
 var memorecto = [];
 var memoverso = [];
-var compteurPair = 0;
+var compteurPaire = 0;
+var nbCoup = 0;
+
+afficheGagner.addEventListener("click", function () {
+    document.location.reload();
+});
+buttonReset.addEventListener("click", function () {
+    document.location.reload();
+});
+
+console.log(compteurPaire);
+
 for (let i = 0; i < recto.length; i++) {
     recto[i].addEventListener("click", function (e) {
         if (check.length < 2) {
             returnImg(e, true);
         }
+        nbCoup++;
+        var afficheNbCoup = (document.getElementById("nbCoup").innerHTML = "Nombre de clics : " + nbCoup);
+        console.log(nbCoup);
         if (check.length == 2) {
             if (check[0] == check[1]) {
-                compteurPair++;
+                compteurPaire++;
+                var affichePaire = (document.getElementById("nbPaire").innerHTML = "Nombre de paire trouver : " + compteurPaire);
                 check = [];
                 memoverso = [];
                 memorecto = [];
+                if (compteurPaire == 1) {
+                    for (let i = 0; i < recto.length; i++) {
+                        recto[i].style.opacity = "0.5";
+                        verso[i].style.opacity = "0.5";
+                    }
+                    afficheGagner.style.display = "flex"
+                }
             } else {
-                timeout = setTimeout(returnImg, 3000, e, false);
+                timeout = setTimeout(returnImg, 1000, e, false);
             }
             etat = false;
         }
     });
 }
 
-function returnImg(e, etat) {
+function returnImg(e, etat, compteurPaire) {
     if (check.length == 2) {
         if (check[0] == check[1]) {
             check = [];
         }
-        etat = false
+        etat = false;
     }
     var imgCliquer = e.target.parentNode.getElementsByClassName("verso")[0];
     if (etat == true) {
@@ -38,39 +69,67 @@ function returnImg(e, etat) {
         memoverso.push(imgCliquer);
         memorecto.push(e.target);
     } else {
-        memorecto[0].style.display = "flex"
-        memorecto[1].style.display = "flex"
-        memoverso[0].style.display = "none"
-        memoverso[1].style.display = "none"
+        memorecto[0].style.display = "flex";
+        memorecto[1].style.display = "flex";
+        memoverso[0].style.display = "none";
+        memoverso[1].style.display = "none";
         check = [];
         memorecto = [];
         memoverso = [];
     }
-
 }
 
-var button = document.getElementById("solution");
 button.addEventListener("click", solution);
-var sol = false;
+
 function solution() {
-
-    var listeRecto = document.getElementsByClassName("recto")
-    var listeVerso = document.getElementsByClassName("verso")
+    var listeRecto = document.getElementsByClassName("recto");
+    var listeVerso = document.getElementsByClassName("verso");
     for (let i = 0; i < listeRecto.length; i++) {
-
         if (sol == false) {
             listeRecto[i].style.display = "none";
             listeVerso[i].style.display = "flex";
-
         } else if (sol == true) {
             listeRecto[i].style.display = "flex";
             listeVerso[i].style.display = "none";
         }
-
     }
     if (sol == false) {
         sol = true;
     } else {
         sol = false;
     }
+}
+
+var min = 0;
+var sec = 0;
+var secTemp = 0;
+
+//CHRONO
+var chrono = document.getElementById("chrono");
+function timer() {
+    sec++;
+    if (sec >= 60) {
+        if (secTemp != 0 && sec > 55) {
+            secTemp = sec - secTemp;
+            sec = secTemp;
+            min++;
+            secTemp = 0;
+        } else {
+            sec = 0;
+            min++;
+            secTemp = 0;
+        }
+    }
+    if (min < 10) {
+        time = "0" + min;
+    } else {
+        time = min;
+    }
+    if (sec < 10) {
+        time += ":0" + sec;
+    } else {
+        time += ":" + sec;
+    }
+    chrono.innerHTML = time;
+    horloge = setTimeout("timer()", 1000);
 }
