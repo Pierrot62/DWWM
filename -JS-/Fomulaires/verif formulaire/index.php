06704c9a0e34c0246918ede17db03abb2b1745a7
Pierrot62 <courquin.pierre62@gmail.com>
2021-01-04 17:15:04 +0100

    04/01/2021 17:15:04,05

--- -JS-/Fomulaires/verif formulaire/index.php
+++ -JS-/Fomulaires/verif formulaire/index.php
@@ -10,40 +10,43 @@
 
 <body>
 
-
-    <div id="fomulaire">
-        <h1>Formulaire</h1>
-        <form action="" method="POST">
-            <div class="elmForm">
-                <label for="nom">Nom :</label>
-                <input id="nom" name="nom" type="text" required>
-                <span></span>
-                <span id="spanNom"></span>
-            </div>
-            <div class="elmForm">
-                <label for="ddn">Date de naissance :</label>
-                <input id="ddn" name="ddn" type="text" required>
-                <span></span>
-                <div>
-                    <span id="spanNom"></span>
-                </div>
+       <div class="content">
+     <fieldset>
+      <legend>Formulaire informatif</legend>
+      <form>
+        
+        <div class="elmForm">
+          <label for="Nom">Nom :</label> 
+          <input type="text" name="nom" id="nom" required placeholder="Votre nom"/>
+          <span id="spanNom"><i title="Entrez votre nom : minimun 3 lettres, pas de chiffres" class="question fas fa-question"></i></span>
+        </div>
 
 
-            </div>
-            <div class="elmForm">
-                <label for="codePostal">Code postal :</label>
-                <input id="cp" name="codePostal" type="text" required>
-                <span></span>
-                <span id="spanCp"></span>
+        <div class="elmForm">
+          <label for="ddn">Date de naissance :</label> 
+          <input type="date" name="ddn" id="ddn" required placeholder="Date de naissance" />
+          <span id="spanDdn"><i title="Entrez votre date de naissance" class="question fas fa-question"></i></span>
+        </div>
 
-            </div>
-            <div class="elmForm">
-                <input type="button" value="Envoyer">
-            </div>
-        </form>
+
+        <div class="elmForm">
+          <label for="cp">Code postal :</label> 
+          <input title="5 Chiffres attendus" type="text" name="cp" id="cp" required placeholder="XXXXX"/>
+          <span id="spanCp"><i title="Entrez votre code postal : 5 chiffres" class="question fas fa-question"></i></span>
+        </div>
+
+      <p class="info">* : Informations obligatoire</p>
+      <p class="info" id="info"></p>
+        <div class="center">
+          <input id="button" type="submit" disabled value="Envoyer"/>
+        </div>
+
+
+      </form>
+    </fieldset>
     </div>
 
-
+    <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
     <script src="script.js"></script>
 </body>
 
--- -JS-/Fomulaires/verif formulaire/script.js
+++ -JS-/Fomulaires/verif formulaire/script.js
@@ -1,47 +1,82 @@
 var inputName = document.getElementById("nom");
 var inputDdn = document.getElementById("ddn");
 var inputCp = document.getElementById("cp");
+var button = document.getElementById("button");
+var spaninfo = document.getElementById("info")
 
 var spanNom = document.getElementById("spanNom");
-// var spanNom = document.getElementById("spanNom");
+var spanDdn = document.getElementById("spanNom");
 var spanCp = document.getElementById("spanCp");
 
-inputName.addEventListener('keyup', verifNom)
-inputDdn.addEventListener('keyup', verifDdn)
-inputCp.addEventListener('keyup', verifCp)
+inputName.addEventListener("keyup", verifNom);
+inputDdn.addEventListener("change", verifDdn);
+inputCp.addEventListener("keyup", verifCp);
 
+var compteurVerif = 0;
 
 function verifNom() {
-    let content = inputName.value;
-    let lettre = content.charAt(content.length - 1);
+  let content = inputName.value;
+  let lettre = content.charAt(content.length - 1);
 
-    if (!isNaN(lettre)) {
-        inputName.value = inputName.value.slice(0, -1);
-        spanNom.textContent = 'UNIQUEMENT DES LETTRES !';
-    } else {
-        spanNom.textContent = ' ';
-    }
+  if (!isNaN(lettre)) {
+    inputName.value = inputName.value.slice(0, -1);
+    inputName.style.border = "5px solid red";
+    spaninfo.textContent = "Erreur : le nom ne peut comporter que des lettres.";
+  } else if (content.length < 3) {
+    inputName.style.border = "5px solid orange";
+    spaninfo.textContent = "Erreur : le nom doit contenir au moins 3 lettres.";
+  } else {
+    inputName.style.border = "5px solid green";
+    spaninfo.textContent = "";
+    compteurVerif++;
+    activButton(compteurVerif);
+  }
 }
 
 function verifDdn() {
+  let dateUser = inputDdn.value;
+  let jour = parseInt(dateUser.substring(8, 10));
+  let mois = parseInt(dateUser.substring(5, 7));
+  let année = parseInt(dateUser.substring(0, 4));
 
+  let date = new Date(année, mois - 1, jour);
+  let dateSysteme = new Date();
+  if (date > dateSysteme) {
+    inputDdn.style.border = "5px solid red";
+    spaninfo.textContent = "Erreur : la date de naissance ne peux pas être supérieur a la date actuelle.";
+  } else {
+    inputDdn.style.border = "5px solid green";
+    spaninfo.textContent = "";
+    compteurVerif++;
+    activButton(compteurVerif);
+  }
 }
 
 function verifCp() {
-    let content = inputCp.value;
-    let lettre = content.charAt(content.length - 1);
+  let content = inputCp.value;
+  let lettre = content.charAt(content.length - 1);
 
-    if (isNaN(lettre)) {
-        inputCp.value = inputCp.value.slice(0, -1);
-        spanCp.textContent = 'UNIQUEMENT DES CHIFFRES !';
-    }else if(content.length > 5){
-        inputCp.value = inputCp.value.slice(0, -1);
-        spanCp.textContent = '5 chiffre attendu uniquement !'
-    }
-    else {
-        spanCp.textContent = ' ';
-    }
+  if (isNaN(lettre)) {
+    inputCp.value = inputCp.value.slice(0, -1);
+    inputCp.style.border = "5px solid red";
+    spaninfo.textContent = "Erreur : le code postal ne peut contenir que des chiffres.";
+  } else if (content.length > 5) {
+    inputCp.value = inputCp.value.slice(0, -1);
+  } else if (content.length < 5) {
+    inputCp.style.border = "5px solid orange";
+  } else {
+    inputCp.style.border = "5px solid green";
+    spaninfo.textContent = "";
+    compteurVerif++;
+    activButton(compteurVerif);
+  }
 }
 
-    // var tabDdn = inputDdn.value;
-    // var tabCp = inputCp.value;
+function activButton(compteurVerif) {
+  if (compteurVerif == 3) {
+    button.style.opacity = "1";
+    button.removeAttribute("disabled", "");
+  } else {
+    
+  }
+}
