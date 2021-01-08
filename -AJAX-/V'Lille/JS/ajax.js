81b294017e145bdb8e4fe35bdc88dbbbb22b7c4f
Pierrot62 <courquin.pierre62@gmail.com>
2021-01-08 17:15:03 +0100

    08/01/2021 17:15:03,06

--- -AJAX-/V'Lille/JS/ajax.js
+++ -AJAX-/V'Lille/JS/ajax.js
@@ -1,6 +1,39 @@
+//recup des champs
+
 // Utilisation de l'Ajax pour appeler l'API et récuperer les enregistrements
 var contenu = document.getElementById("contenu");
 var enregs; // contient la reponse de l'API
+
+//fonction de tri du tableau
+function TriStations(a, b) {
+    if (a.fields.commune<b.fields.commune)
+    {
+       return -1;
+    }
+    else{
+        if (a.fields.commune > b.fields.commune)
+        {
+            return 1;
+        }
+        else{
+            if (a.fields.nom<b.fields.nom)
+            {
+                return -1;
+            }
+            else{
+                if (a.fields.nom>b.fields.nom)
+                {
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+        }
+    }
+  }
+
+
 // on définit une requete
 const req = new XMLHttpRequest();
 //on vérifie les changements d'états de la requête
@@ -13,11 +46,14 @@ req.onreadystatechange = function (event) {
             console.log(this.responseText);
             console.log(reponse);
             enregs = reponse.records;
+            enregs=enregs.sort(TriStations);
             for (let i = 0; i < enregs.length; i++) {
                 // on crée la ligne et les div internes
                 ligne = document.createElement("div");
                 ligne.setAttribute("class", "ligne");
                 ligne.id = i;
+                divIco = document.createElement("i");
+                ligne.appendChild(divIco);
                 commune = document.createElement("div");
                 commune.setAttribute("class", "commune");
                 ligne.appendChild(commune);
@@ -32,6 +68,12 @@ req.onreadystatechange = function (event) {
                 espace.setAttribute("class","espaceHorizon");
                 contenu.appendChild(espace);
                 //on met à jour le contenu
+                if (enregs[i].fields.etat ==  "EN SERVICE") {
+                    divIco.setAttribute("class", "fas fa-check vert");
+                } else {
+                    divIco.setAttribute("class", "fas fa-times rouge");
+                    ligne.style.opacity = ("0.5");
+                }
                 commune.innerHTML = enregs[i].fields.commune;
                 libelle.innerHTML = enregs[i].fields.nom;
                 etat.innerHTML = enregs[i].fields.etat;
@@ -51,27 +93,26 @@ function afficheDetail(e) {
     veloClique.removeEventListener("click", afficheDetail);
     detail = document.createElement("div");
     detail.setAttribute("class", "detail");
-    ico = document.createElement("div");
-    ico.setAttribute("class", "grand");
-    ico.setAttribute("class", "ico");
     adresse = document.createElement("div");
     adresse.setAttribute("class", "adresse");
     detail.appendChild(adresse);
     dispo = document.createElement("div");
     dispo.setAttribute("class", "dispo");
     detail.appendChild(dispo);
+    // ico = document.createElement("i");
+    // ico.setAttribute("class", "fas fa-bicycle");
+    // ico.appendChild(nbvelosdispo);
     nbvelosdispo = document.createElement("div");
     nbvelosdispo.setAttribute("class", "nbvelosdispo");
     detail.appendChild(nbvelosdispo);
-    if (enregs[veloClique.id].fields.adresse == "OUVERT") {
-        ico.innerHTML = enregs[veloClique.id].fields.adresse;
-    }
     adresse.innerHTML = enregs[veloClique.id].fields.adresse;
     dispo.innerHTML = enregs[veloClique.id].fields.etat;
-    nbvelosdispo.innerHTML= "  Nb de velo max : " + enregs[veloClique.id].fields.nbvelosdispo;
+    nbvelosdispo.innerHTML= "  Nb de velo disponible : " + enregs[veloClique.id].fields.nbvelosdispo;
     contenu.insertBefore(detail, veloClique.nextSibling);
 }
 
+// maj.innerHTML= "mis a jour : " + enregs.facet.datemiseajour;
+
 //on envoi la requête
-req.open('GET', 'https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion', true);
+req.open('GET', 'https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=50&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&facet=datemiseajour', true);
 req.send(null);
--- -AJAX-/V'Lille/PHP/View/Principale.php
+++ -AJAX-/V'Lille/PHP/View/Principale.php
@@ -1,10 +1,7 @@
 ﻿<!-- Cette page est le contenu de la page principale du site; Elle présente la liste des parkings. -->
-
+<div class="maj">qsdqsd</div>
 <div id="divContenu">
 	<div class="ligne titreLigne">
-		<div class="ico">
-		<i class="fas fa-times"></i>
-		</div>
 		<div class="commune grand">Commune</div>
 		<div class="libelle grand">Nom de l'emplacement</div>
 		<div class="etat grand">Etat</div>
