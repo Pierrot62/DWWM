1f226d1d220dbf6fdd44a929f0c0e7e4c184ae98
Pierrot62 <courquin.pierre62@gmail.com>
2021-01-19 21:55:29 +0100

    vjb,

--- projet/Conventions/PHP/VIEW/FormFRStagiaire.php
+++ projet/Conventions/PHP/VIEW/FormFRStagiaire.php
@@ -3,7 +3,6 @@
     //ACTION DES BOUTONS DE SELECTION DE PERIODES DE STAGES//
 
 
-
     $utilisateur = new Utilisateurs (["idUtilisateur" => 2, "nomUtilisateur" => "COURQUIN", "prenomUtilisateur" => "Pierre", "emailUtilisateur" => "toto6@test.fr" , "mdpUtilisateur" => "user" , "datePeremption" => "", "idRole" => 4]);
     // $a1 = new agence(["Nom" => "tutu", "adresse" => "12 rue toto","restauration" => "restaurant d'entreprise" ,"codePostal" => "59520" , "ville" => "Lille"]);
     $_SESSION['utilisateur'] = $utilisateur;
@@ -14,30 +13,113 @@
         // var_dump($stagiaire);
         $participation = ParticipationsManager::getByStagiaire($stagiaire->getIdStagiaire());
         // var_dump($participation);
-        $periodeStage = StagiaireFormationManager::nb
+        $periodeStage = StagiaireFormationManager::getListByStagiaire($stagiaire->getIdStagiaire());
+        // var_dump($periodeStage);
     }
     echo' <section>';
         // $nbPeriodesStages = 5;
-    if ($nbPeriodesStages > 1) {
+    if (!isset($_GET['idPeriode']) && $nbPeriodesStages > 1) {
         $i = 0;
         $j = 1;
         while ($i < $nbPeriodesStages) {
+            //DECOUPAGE DE LA DATE POUR LA METTRE AU FORMAT JJ/MM/AAAA
+            $dateDebut = $periodeStage[$i]->getDateDebutPAE();
+            $anneeDebut = substr($dateDebut , 0 , 4);
+            $moisDebut = substr($dateDebut, 5 , 2);
+            $jourDebut = substr($dateDebut, 8 , 2);
+            $dateFin = $periodeStage[$i]->getDateFinPAE();
+            $anneeFin = substr($dateFin , 0 , 4);
+            $moisFin = substr($dateFin, 5 , 2);
+            $jourFin = substr($dateFin, 8 , 2);
+        
             echo '
-            <div class="colonne">
+            <div class="colonne center">
                 <div>Periode de stage '. $j.'</div>
-                <div>Du '. $participation[$i]->getDateDebut().' au  '. $participation[$i]->getDateFin().'</div>
+                <div>Du '.$jourDebut.'/'.$moisDebut.'/'.$anneeDebut.' au  '.$jourFin.'/'.$moisFin.'/'.$anneeFin.' </div>
 
-                <a href="index.php?page=FormFRStagiaire&idPeriodStage='.$part">
+                <a class="centre" href="index.php?page=FormFRStagiaire&idPeriode='.$periodeStage[$i]->getIdPeriode().'">
                     <button type="submit" class="bouton">Completer la fiche info  </button>
                 </a>
             </div>
             ';
-
             $i++; 
             $j++;
         }
+        } else if (isset($_GET["idPeriode"])){
+            $valuePeriodeStage = StagiaireFormationManager::getListBySession($_GET["idPeriode"]);
+            // var_dump($valuePeriodeStage);
+             //DECOUPAGE DE LA DATE POUR LA METTRE AU FORMAT JJ/MM/AAAA
+            //  $dateDebut = $valuePeriodeStage->getDateDebutPAE();
+            //  $anneeDebut = substr($dateDebut , 0 , 4);
+            //  $moisDebut = substr($dateDebut, 5 , 2);
+            //  $jourDebut = substr($dateDebut, 8 , 2);
+            //  $dateFin = $valuePeriodeStage->getDateFinPAE();
+            //  $anneeFin = substr($dateFin , 0 , 4);
+            //  $moisFin = substr($dateFin, 5 , 2);
+            //  $jourFin = substr($dateFin, 8 , 2);
+            // <span>Du '.$jourDebut.'/'.$moisDebut.'/'.$anneeDebut.' au  '.$jourFin.'/'.$moisFin.'/'.$anneeFin.' </span>
+            echo'
+            <form action="" method="POST">
+                <div class="info centre colonne">
+                   <h1>Fiche d information pour la periode de stage :</h1>
+                  
+                </div>
+                <div class="info colonne">
+                    <div class="info colonne ">
+                        <label for="prenom">Prenom :</label>
+                        <input type="text" id="prenom" name="prenom" value="" required pattern="[a-zA-Z- ]{3,}">
+                    </div>
+                    <div class="info colonne ">
+                        <label for="nom">Nom :</label>
+                        <input type="text" id="nom" name="nom" value="" required pattern="[a-zA-Z- ]{3,}">
+                    </div>
+                </div>
+                <div >
+                    <div class="info  centerItem colonne">
+                        <label for="homme">Homme</label>
+                        <input type="radio" required id="genre" name="genre" value="H">
+                    </div>
+                    <div class="info  centerItem colonne">
+                        <label for="femme">Femme</label>
+                        <input type="radio" required id="genre" name="genre" value="F">
+                    </div>
+                    <div class="info colonne  grande">
+                        <label for="numSecu">Votre Numero de securite social :</label>
+                        <input type="text" id="numSecu" name="numSecu" required pattern="\d{13}" value="">
+                    </div>
+                </div>
 
-    } else {
+                <div >
+                    <div class="info colonne center">
+                        <label for="numbenef">Votre numero de beneficiaire :</label>
+                        <input type="text" id="numBenef" name="numbenef" value="" required pattern="\d{8}">
+                    </div>
+                    <div class="info colonne center">
+                        <label for="ddn">Votre date de naissance :</label>
+                        <input type="date" id="ddn" name="ddn" value="" required>
+                    </div>
+                </div>
+
+                <div >
+                    <div class="info colonne center">
+                        <label for="emailTuteur">Email de votre tuteur :</label>
+                        <input type="text" id="emailTuteur" name="emailTuteur" value="" required pattern="^[a-z]+[a-z0-9._-]+@[a-z0-9._-]+\.[a-z]{2,6}$" >                
+                    </div>
+                </div>
+                <div >
+                    <div class="info colonne center">
+                        <button class="bouton" type="submit"><i class="fas fa-paper-plane"></i> Envoyer</button>
+                    </div>
+                </div>
+                <div >
+                    <div class="info center">
+                        <span class="erreur"></span>
+                    </div>
+                </div>
+
+            </form>';
+        } 
+        else {
 
         echo ' 
        
@@ -45,7 +127,7 @@
             
             <form action="" method="POST">
 
-                <div class=" ">
+                <div class="info colonne">
                     <div class="info colonne ">
                         <label for="prenom">Prenom :</label>
                         <input type="text" id="prenom" name="prenom" value="" required pattern="[a-zA-Z- ]{3,}">
