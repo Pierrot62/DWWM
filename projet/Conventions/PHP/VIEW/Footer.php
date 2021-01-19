b3d5f44d5887e0dc46366f43abde45b2422a8016
Pierrot62 <courquin.pierre62@gmail.com>
2021-01-19 12:15:03 +0100

    19/01/2021 12:15:03,50

--- projet/Conventions/PHP/VIEW/Footer.php
+++ projet/Conventions/PHP/VIEW/Footer.php
@@ -1,1 +1,19 @@
-<script src="./JS/script.js"></script>
+<div></div>
+</div>
+<footer>
+    <h3>&copy;DWWM 2020</h3>
+</footer>
+<?php 
+if (isset($page))
+{
+    switch ($page[1])
+    {
+        case "FormFRStagiaire" : echo '<script src="./JS/VerifFormStagiaire.js"></script>';break;
+        case "ListeUtilisateurs" : echo '<script src="./JS/FiltreUtilisateurs.js"></script>';break;
+
+    }
+}
+      ?>
+</body>
+
+</html>
--- projet/Conventions/PHP/VIEW/Head.php
+++ projet/Conventions/PHP/VIEW/Head.php
@@ -1,9 +1,11 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $titre ?></title>
-	<link rel="stylesheet" href="./CSS/style.css">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
-    integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./CSS/Root.css">
+    <link rel="stylesheet" href="./CSS/Style.css">
+    <title>Gestion des conventions</title>
 </head>
+<body class="colonne">
