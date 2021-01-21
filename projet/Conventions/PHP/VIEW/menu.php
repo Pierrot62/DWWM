d3c54914fdcae87147ee3cafdb07d9b3763d33d4
Pierrot62 <courquin.pierre62@gmail.com>
2021-01-21 17:43:44 +0100

    21/01/2021 17:43:44,22

--- projet/Conventions/PHP/VIEW/menu.php
+++ projet/Conventions/PHP/VIEW/menu.php
@@ -1,7 +1,7 @@
 <section>
 
     <div class="menu centre">
-        <div class="elmMenu"><a  class="centerItem double lien" href="index.php?page=Menu">Informations Stagiaires</a></div>
+        <div class="elmMenu"><a  class="centerItem double lien" href="index.php?page=FormFRStagiaire">Informations Stagiaires</a></div>
         <div class="elmMenu"><a  class="centerItem double lien" href="index.php?page=FormFREntreprise">Informations Entreprise</a></div>
         <div class="elmMenu"><a  class="centerItem double lien" href="index.php?page=FormFRConditionStage">Conditions <br>de stage</a></div>
         <div class="elmMenu"><a  class="centerItem double lien" href="index.php?page=FormFRSujetStage">Sujet de<br> Stage</a></div>
