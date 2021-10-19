8844c29ef71e76378be4bafc322740153c9b258d
Pierre62 <courquin.pierre62@gmail.com>
2021-10-19 17:27:59 +0200

    19/10/2021 17:27:59,79

--- projet/Conventions/PHP/VIEW/Head.php
+++ projet/Conventions/PHP/VIEW/Head.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/ce4feb7268.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./CSS/Root.css">
-    <link rel="stylesheet" href="./CSS/Style.css">
-    <title>Gestion des conventions</title>
-</head>
-<body class="colonne">
+db.modeles.insert({ "mod_nom": "C4",
+ "mod_marque": "Citroën", 
+"mod_cylindree": 1.0,
+"mod_prix": 18470,
+"mod_date_ajout": new Date(), 
+"mod_d_dispo": new Date("2018-10-20"), 
+ "mod_options": [ "GPS", "Toit ouvrant", "Peinture métallisée" ] 
+ })
+
+ 
